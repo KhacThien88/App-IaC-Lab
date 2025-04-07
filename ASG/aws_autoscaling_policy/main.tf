92187bd353e1b24b14b14b051f484cb778556c42
KhacThien88 <KTeightop1512@gmail.com>
2025-04-07 23:46:16 +0700

    update metric cloudwatch monitoring auto-scaling-group

--- ASG/aws_autoscaling_policy/main.tf
+++ ASG/aws_autoscaling_policy/main.tf
@@ -1,27 +1,48 @@
-resource "aws_autoscaling_policy" "scale_out" {
+resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale-out-on-high-cpu"
   autoscaling_group_name = var.asg_name
   adjustment_type        = "ChangeInCapacity"
-  policy_type           = "StepScaling"
-
-  step_adjustment {
-    scaling_adjustment          = 1
-    metric_interval_lower_bound = var.target_scale_up
-  }
+  scaling_adjustment     = 1
+  cooldown               = 60
 }
 
-resource "aws_autoscaling_policy" "scale_in" {
+resource "aws_autoscaling_policy" "scale_down" {
   name                   = "scale-in-on-low-cpu"
   autoscaling_group_name = var.asg_name
   adjustment_type        = "ChangeInCapacity"
-  policy_type            = "StepScaling"
+  scaling_adjustment     = -1
+  cooldown               = 300
+}
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "cpu_high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 70
+  alarm_description   = "Kích hoạt khi CPU vượt quá 70%"
+  alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
 
-  step_adjustment {
-    scaling_adjustment          = -1
-    metric_interval_upper_bound = var.target_scale_down
+  dimensions = {
+    AutoScalingGroupName = var.asg_name
   }
-  step_adjustment {
-    scaling_adjustment          = -1
-    metric_interval_upper_bound = 0
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_low" {
+  alarm_name          = "cpu_low"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 30
+  alarm_description   = "Kích hoạt khi CPU dưới 30%"
+  alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
+
+  dimensions = {
+    AutoScalingGroupName = var.asg_name
   }
 }
