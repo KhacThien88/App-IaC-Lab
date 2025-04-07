cc8fea177cdc88641c6afdf69b47faa42eb6381c
KhacThien88 <KTeightop1512@gmail.com>
2025-04-07 17:19:12 +0700

    delete cooldown in target scaling

--- ASG/aws_autoscaling_policy/main.tf
+++ ASG/aws_autoscaling_policy/main.tf
@@ -1,28 +1,23 @@
-resource "aws_autoscaling_policy" "scale_up_policy" {
-  name                   = "scale-up-policy"
-  autoscaling_group_name   = var.asg_name
-  policy_type             = "StepScaling"
-
-  adjustment_type         = "ChangeInCapacity"
-
-  step_adjustment {
-    metric_interval_lower_bound = var.target_scale_up
-    scaling_adjustment          = 1
-  }
-
-  cooldown                 = 300
-}
-resource "aws_autoscaling_policy" "scale_down_policy" {
-  name                   = "scale-down-policy"
-  autoscaling_group_name   = var.asg_name
-  policy_type            = "StepScaling"
-
+resource "aws_autoscaling_policy" "scale_out" {
+  name                   = "scale-out-on-high-cpu"
+  autoscaling_group_name = var.asg_name
   adjustment_type        = "ChangeInCapacity"
+  policy_type           = "StepScaling"
 
   step_adjustment {
-    metric_interval_upper_bound = var.target_scale_down
-    scaling_adjustment          = -1
+    scaling_adjustment          = 1  # Add 1 instance
+    metric_interval_lower_bound = var.target_scale_up
   }
+}
 
-  cooldown                 = 300
+resource "aws_autoscaling_policy" "scale_in" {
+  name                   = "scale-in-on-low-cpu"
+  autoscaling_group_name = var.asg_name
+  adjustment_type        = "ChangeInCapacity"
+  policy_type           = "StepScaling"
+
+  step_adjustment {
+    scaling_adjustment          = -1
+    metric_interval_upper_bound = var.target_scale_down
+  }
 }
